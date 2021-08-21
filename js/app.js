bd30ef829bb12b1d977b40fd29b99fdc98c25afc
Stelchic <stilyanmtoskov@gmail.com>
2021-08-21 12:46:14 +0100

    Search Function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     baguetteBox.run('.gallery', {
         animation: 'fadeIn',
         overlayBackgroundColor: 'rgba(142,43,232,0.5)'
     });
-  });
+});
+const search = () => {
+    let input = document.getElementById('search').value;
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('photos');
+    for (let i = 0; i < x.length; i++) {
+        if (!x[i].getAttribute('data-caption').includes(input)) {
+            x[i].style.display = "none";
+        } else {
+            x[i].style.display = "list-item";
+        }
+    }
+}
